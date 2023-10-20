5e80347c912e2c565e3204646f6f726303ca1b99
vincula1 <141301612+vincula1@users.noreply.github.com>
2023-10-20 17:13:12 -0500

    Added generator for file and init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 
@@ -49,11 +50,35 @@ const questions = [
     },
   ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  // Generate the README content based on the 'data' object
+  const readmeContent = generateMarkdown(data);
+
+  // Write the content to the specified file
+  fs.writeFile(fileName, readmeContent, (err) => {
+    if (err) {
+      console.error('Error writing README file:', err);
+    } else {
+      console.log(`README file '${fileName}' created successfully!`);
+    }
+  });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // Prompt the user with the questions
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Call the writeToFile() function to generate the README file
+      writeToFile('README.md', answers);
+    })
+    .catch((error) => {
+      console.error('Error occurred:', error);
+    });
+}
 
 // Function call to initialize app
 init();
